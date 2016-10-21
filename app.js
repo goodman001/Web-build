94b0829a4cbc70e5932bc66179b7d926450c1ad6
u <u@us-Mac.local>
2016-10-21 09:53:36 +0800

    register before

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/register',routes);
+app.use('/userinfo',routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,28 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Personal Infomation' });
 });
 
+router.post('/userinfo', function(req, res, next) {
+  var name = req.body.name;
+  var age = req.body.age;
+  var gender = req.body.inlineRadioOptions;
+  var occupation = req.body.occupation;
+  var hometown = req.body.hometown;
+  var ops = new Array();
+  var op;
+  ops[0] = req.body.inlineCheckbox0;
+  ops[1] = req.body.inlineCheckbox1;
+  ops[2] = req.body.inlineCheckbox2;
+  ops[3] = req.body.inlineCheckbox3;
+  ops[4] = req.body.inlineCheckbox4;
+  for (op in ops)
+  {
+     if(!ops[op])
+     {
+        ops[op] = 0;
+     }
+  }
+  console.log(ops[1]);
+  //res.render('register', { title: 'Personal Infomation' });
+  });
+
 module.exports = router;
